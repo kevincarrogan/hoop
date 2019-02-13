f3e9ad85525311fd9b0f3acf11d7b7c8b5741229
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-02-13 23:02:19 +0000

    Style form.

--- src/App.js
+++ src/App.js
@@ -15,30 +15,34 @@ class App extends Component {
         </header>
         <section>
           <h1>About your activity</h1>
-          <form>
-            <div>
+          <form className="base-form">
+            <div className="form-field">
               <label>Activity Name</label>
               <input type="text" name="activity_name" />
             </div>
-            <div>
+            <div className="form-field">
               <label>Recommended Age</label>
               <select>
+                <option>From</option>
                 <option>2</option>
                 <option>3</option>
               </select>
               <select>
+                <option>To</option>
                 <option>2</option>
                 <option>3</option>
               </select>
             </div>
-            <div>
+            <div className="form-field">
               <label>Activity Webpage</label>
-              <p>Use a specific page if possible. Try to avoid homepage links.</p>
+              <p className="instructions">Use a specific page if possible. Try to avoid homepage links.</p>
               <input type="text" name="activity_webpage" placeholder="e.g. example.com/actiivty" />
             </div>
-            <div>
-              <label>Activity Phone Number</label>
-              <span>Optional</span>
+            <div className="form-field">
+              <div className="label-wrapper">
+                <label>Activity Phone Number</label>
+                <span className="optional">Optional</span>
+              </div>
               <input type="text" name="activity_phone_number" />
             </div>
           </form>
