63db927a1be1f97b077e4d27aadd76994bcc4a3d
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-02-16 14:27:11 +0000

    Abstract form field into separate component.
    Currently only swaps out for one input.

--- src/components/AboutActivityForm.js
+++ src/components/AboutActivityForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { reduxForm, Form, Field } from 'redux-form';
 
+import classNames from 'classnames';
+
 import './Form.css';
 
 const validate = values => {
@@ -34,18 +36,38 @@ const validate = values => {
   return errors;
 };
 
+let FormField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error }
+}) => {
+  let className = classNames('form-field', {error: touched && error});
+  let inputElement;
+
+  switch (type) {
+    default:
+      inputElement = <input {...input} id={input.name} type={type} />;
+  }
+
+  return (
+    <div className={className}>
+      <label htmlFor={input.name}>{label}</label>
+      {inputElement}
+      {error && touched && <div className="error-message">{error}</div>}
+    </div>
+  );
+};
+
 let AboutActivityForm = ({ id, handleSubmit, onSubmit }) => {
   return (
     <>
       <h1>About your activity</h1>
       <Form id={id} className="base-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-field">
-          <label htmlFor="name">Activity Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
+        <Field label="Activity Name" name="name" component={FormField} type="text" />
         <div className="form-field">
           <label>Recommended Age</label>
-          <Field name="recommendedAgeFrom" component="select">
+          <Field id="recommendedAgeFrom" name="recommendedAgeFrom" component="select">
             <option>From</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -59,14 +81,14 @@ let AboutActivityForm = ({ id, handleSubmit, onSubmit }) => {
         <div className="form-field">
           <label htmlFor="webpage">Activity Webpage</label>
           <p className="instructions">Use a specific page if possible. Try to avoid homepage links.</p>
-          <Field name="webpage" component="input" type="text" placeholder="e.g. example.com/activity" />
+          <Field id="webpage" name="webpage" component="input" type="text" placeholder="e.g. example.com/activity" />
         </div>
         <div className="form-field">
           <div className="label-wrapper">
             <label htmlFor="phoneNumber">Activity Phone Number</label>
             <span className="optional">Optional</span>
           </div>
-          <Field name="phoneNumber" component="input" type="text" />
+          <Field id="phoneNumber" name="phoneNumber" component="input" type="text" />
         </div>
       </Form>
     </>
