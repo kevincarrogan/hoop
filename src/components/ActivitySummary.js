fa0cc2041fdf4d721ddef2159919f85e60fc0d98
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-02-17 11:39:09 +0000

    Add all details to summary page with styling.

--- src/components/ActivitySummary.js
+++ src/components/ActivitySummary.js
@@ -1,22 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import styles from './ActivitySummary.module.css';
+
 const ActivitySummary = ({ activity }) => {
   return <>
-    <h1>Activity summary</h1>
+    <h1>Activity Summary</h1>
 
-    <dl>
-      <dt>Name</dt>
-      <dd>{ activity.name }</dd>
+    <h2 className={styles.secondaryHeader}>Activity</h2>
 
-      <dt>Recommended age</dt>
-      <dd>{ activity.recommendedAgeFrom } - { activity.recommendedAgeTo }</dd>
+    <dl className={styles.summaryDetails}>
+      <dt className={styles.summaryDetailsTitle}>Name</dt>
+      <dd className={styles.summaryDetailsDefinition}>{activity.name}</dd>
 
-      <dt>Webpage</dt>
-      <dd>{ activity.webpage }</dd>
+      <dt className={styles.summaryDetailsTitle}>Recommended Age</dt>
+      <dd className={styles.summaryDetailsDefinition}>{activity.recommendedAgeFrom} - {activity.recommendedAgeTo}</dd>
 
-      <dt>Phone number</dt>
-      <dd>{ activity.phoneNumber }</dd>
+      <dt className={styles.summaryDetailsTitle}>Webpage</dt>
+      <dd className={styles.summaryDetailsDefinition}>{activity.webpage}</dd>
+
+      {activity.phoneNumber &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Phone Number</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.phoneNumber}</dd>
+        </>
+      }
+    </dl>
+
+    <h2 className={styles.secondaryHeader}>Address</h2>
+
+    <dl className={styles.summaryDetails}>
+      <dt className={styles.summaryDetailsTitle}>Place Name</dt>
+      <dd className={styles.summaryDetailsDefinition}>{activity.placeName}</dd>
+
+      <dt className={styles.summaryDetailsTitle}>Postcode</dt>
+      <dd className={styles.summaryDetailsDefinition}>{activity.postcode}</dd>
+
+      {activity.buildingUnit &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Building Unit</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.buildingUnit}</dd>
+        </>
+      }
+
+      {activity.buildingName &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Building Name</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.buildingName}</dd>
+        </>
+      }
+
+      {activity.streetNumber &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Street Number</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.streetNumber}</dd>
+        </>
+      }
+
+      {activity.streetName &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Street Name</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.streetName}</dd>
+        </>
+      }
+
+      {activity.town &&
+        <>
+          <dt className={styles.summaryDetailsTitle}>Town</dt>
+          <dd className={styles.summaryDetailsDefinition}>{activity.town}</dd>
+        </>
+      }
     </dl>
   </>
 };
